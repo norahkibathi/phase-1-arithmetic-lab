22cb2c5ce0bbaeff893733f6536175877da1eb4c
Norah <norah.njeri@student.moringaschool.com>
2024-01-05 10:44:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+ let num1 = 31;
+ let num2 = 2;
+ let multiply = num1*num2;
+ console.log (multiply);
+ let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+let num3 = 20
+let num4 = 5
+let mod =  num3/num4;
+console.log("mod");
+let max = (1, 2, 10, 13, 20)
+console.log (Math.max(max));
---
+++ notes.js
@@ -0,0 +1,11 @@
+//math.ceil -rounds  a number to the nearest intergrer
+//math.floor- rounds a number down to the nearest interger
+//Math.round - rounds to the closest integer (either up or down)
+let num = 12.345;
+console.log(Math.ceil(num)); //13
+console.log(Math.floor(num)); //12
+console.log(Math.round(num)); //12</s>
+//math.max-returns the highest number from the list of arguements 
+//Math.min-returns the lowest Number.
+//generating random numbers 
+//math.random(); generates random numbers which can be used together with math.floor
